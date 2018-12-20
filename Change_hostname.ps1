15c986641b7ff122e4b496411b2c0ee699046ad2
poorleno1 <jarekole@gmail.com>
2018-12-20 11:19:38 +0100

    Update Change_hostname.ps1

--- Change_hostname.ps1
+++ Change_hostname.ps1
@@ -1,4 +1,4 @@
 param([string]$hostIP)
 
-#(Get-Content C:\Zabbix\zabbix_agentd.win.conf) -replace "10.81.24.39",$hostIP | Set-Content C:\Zabbix\zabbix_agentd.win.conf
+(Get-Content C:\Zabbix\zabbix_agentd.win.conf) -replace "10.81.24.39",$hostIP | Set-Content C:\Zabbix\zabbix_agentd.win.conf
 (Get-Content C:\Zabbix\zabbix_agentd.win.conf) -replace "MyValue",$env:computername | Set-Content C:\Zabbix\zabbix_agentd.win.conf
